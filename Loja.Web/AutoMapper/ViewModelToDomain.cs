7d80213aee39800856b3902ece1b73fbd7677ea5
diogoantonio77 <diogoantonio47@hotmail.com>
2020-12-07 21:06:52 -0300

    incrimentando projeto aspnet

---
+++ Loja.Web/AutoMapper/ViewModelToDomain.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Loja.Dominio;
+using Loja.Web.ViewModels.Product;
+using Loja.Web.ViewModels.ProductProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Loja.Web.AutoMapper
+{
+    public class ViewModelToDomain : Profile
+    {
+        protected override void Configure()
+        {
+            Mapper.CreateMap<ProductViewModel, Product>();
+            Mapper.CreateMap<ProductProviderViewModel, ProductProvider>();
+        }
+    }
+}
